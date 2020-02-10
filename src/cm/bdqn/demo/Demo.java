8636ce776935e2d54f337bd15f1c0114e4a603c2
wuyue-admin <60646744+wuyue-admin@users.noreply.github.com>
2020-02-10 15:56:03 +0800

    Update Demo.java

--- src/cm/bdqn/demo/Demo.java
+++ src/cm/bdqn/demo/Demo.java
@@ -2,6 +2,7 @@ package cm.bdqn.demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("我不知道");
+		System.out.println("测试提交");
+		System.out.println("修改");
 	}
 }
